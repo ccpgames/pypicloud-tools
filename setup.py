3505ec13a4d124662e16dd1be96d18d4f81d3075
a-tal <github@talsma.ca>
2015-05-08 15:51:56 -0700

    0.0.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class PyTest(TestCommand):
 
 setup(
     name="pypicloud-tools",
-    version="0.0.2",
+    version="0.0.3",
     author="Adam Talsma",
     author_email="se-adam.talsma@ccpgames.com",
     packages=["pypicloud_tools"],
